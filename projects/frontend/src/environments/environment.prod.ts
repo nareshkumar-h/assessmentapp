e74b11dc2c17fab9ce636028eebe8a6efd168d7d
Naresh Kumar H <nareshkumarh@live.com>
2020-11-25 09:07:31 +0530

    Update environment.prod.ts

--- projects/frontend/src/environments/environment.prod.ts
+++ projects/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL :"https://learn2build.in/api/",
+  API_URL :"https://api.learn2build.in:8443/api/",
   //API_URL:"http://apiknowledgetrackerin.cfapps.io/api/",
   ORG_ID: "spinsoft"
 };
