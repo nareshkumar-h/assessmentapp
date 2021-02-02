e79bf7b5cc698ab5de928d3b0c041840ab3b69a2
Naresh Kumar H <nareshkumarh@live.com>
2021-02-03 01:17:27 +0530

    Update environment.prod.ts

--- projects/frontend/src/environments/environment.prod.ts
+++ projects/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL :"http://api.learn2build.in:8080/api/",
+  API_URL :"https://knowledgetracker-api.herokuapp.com/api/",
   //API_URL:"http://apiknowledgetrackerin.cfapps.io/api/",
   ORG_ID: "spinsoft"
 };
