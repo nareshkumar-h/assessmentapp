d13113113d174205119d3ecb5c78711ddaaa972f
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-24 12:12:31 +0530

    Fixed bug - added edit access to both user and project reviewer

--- projects/project/src/lib/components/view-project-feature/view-project-feature.component.ts
+++ projects/project/src/lib/components/view-project-feature/view-project-feature.component.ts
@@ -7,82 +7,87 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-view-project-feature',
   templateUrl: './view-project-feature.component.html',
-  styleUrls: ['./view-project-feature.component.css']
+  styleUrls: ['./view-project-feature.component.css'],
 })
 export class ViewProjectFeatureComponent implements OnInit {
+  @Input()
+  featureId: number;
 
   @Input()
-  featureId:number;
+  projectId: number;
+  @Input()
+  showSidebar: boolean;
 
   @Input()
-  projectId:number;
-  @Input()
-  showSidebar:boolean;
+  embedded: boolean;
 
-  @Input()
-  embedded:boolean;
+  breadcrumbItems: any = [];
+  isLoggedInUser: boolean;
+  isReviewer: boolean;
 
-  breadcrumbItems:any= [];
-  isLoggedInUser:boolean;
-  isReviewer:boolean;
-
-  constructor(private projectService:ProjectService,private authService:AuthService, private toastr:ToastrService,private route:ActivatedRoute) {
-    this.isReviewer = this.authService.hasRoleAccess("T");
-    console.log("isReviewer:", this.isReviewer);
-    this.route.parent.params.subscribe(params=>{
-      this.projectId = params["projectId"];
-      console.log("ProjectId:"+ this.projectId);
+  constructor(
+    private projectService: ProjectService,
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private route: ActivatedRoute
+  ) {
+    this.isReviewer = this.authService.hasRoleAccess('T');
+    console.log('isReviewer:', this.isReviewer);
+    this.route.parent.params.subscribe((params) => {
+      this.projectId = params['projectId'];
+      console.log('ProjectId:' + this.projectId);
     });
-    this.route.params.subscribe ( params=>{
-      this.featureId  = params["featureId"];      
+    this.route.params.subscribe((params) => {
+      this.featureId = params['featureId'];
       this.showSidebar = true;
     });
-   }
+  }
 
   ngOnInit(): void {
     this.findOne();
   }
 
-  project:any;
-  feature:any;
+  project: any;
+  feature: any;
 
-  mode:string = "view";
-  noOfTasks:number =0;
+  mode: string = 'view';
+  noOfTasks: number = 0;
 
-  findOne(){
-    this.projectService.findByFeatureId(this.featureId).subscribe (res=>{
-      this.feature  =res;      
-      this.isLoggedInUser = this.authService.getSelectedUser() == this.feature["createdBy"];
+  findOne() {
+    this.projectService.findByFeatureId(this.featureId).subscribe((res) => {
+      this.feature = res;
+      this.isLoggedInUser =
+        this.authService.getSelectedUser() == this.feature['assignedTo'] ||
+        this.authService.getSelectedUser() == this.feature['createdBy'];
       this.project = this.feature.projectModule.project;
     });
   }
 
-  updateFeatureStatus(featureId,status){
-    this.projectService.updateFeatureStatus(featureId, status).subscribe(res=>{
-      this.toastr.success("Successfully Updated");
-      this.ngOnInit();
-    });
+  updateFeatureStatus(featureId, status) {
+    this.projectService
+      .updateFeatureStatus(featureId, status)
+      .subscribe((res) => {
+        this.toastr.success('Successfully Updated');
+        this.ngOnInit();
+      });
   }
 
-
-  update(feature){
-    console.log("Description:" , feature.description);
-    this.projectService.updateFeature( feature).subscribe (res=>{
+  update(feature) {
+    console.log('Description:', feature.description);
+    this.projectService.updateFeature(feature).subscribe((res) => {
       console.log(res);
       this.mode = null;
     });
   }
 
-  deleteFeature(id){
-
-    let cfm = confirm("Do you want to delete the feature?");
-    if(cfm){
-      this.mode = "view";
-      this.projectService.deleteFeature( id).subscribe(res=>{
-        this.toastr.success("Successfully Deleted");
+  deleteFeature(id) {
+    let cfm = confirm('Do you want to delete the feature?');
+    if (cfm) {
+      this.mode = 'view';
+      this.projectService.deleteFeature(id).subscribe((res) => {
+        this.toastr.success('Successfully Deleted');
         history.back();
-      })
+      });
     }
   }
-
 }
